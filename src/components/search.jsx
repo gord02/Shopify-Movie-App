19503b54139f3a57efa32643fd47551b218d79ec
Gordon Hamilton <gordon@Gordons-MacBook-Air.local>
2020-12-24 14:04:34 -0500

    nomination can now be made but cannot be removed from the list

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -12,11 +12,12 @@ class Search extends Component {
         this.removeNomination = this.removeNomination.bind(this);
         // this.Print = this.Print.bind(this);
         this.state = {
+            movie: {},
             title:'',
             year:'',
             plot:'',
-      nominations:[],
-            isNominated: false
+            nominations:[],
+            isNominated: {}
         };
     };
 
@@ -34,21 +35,47 @@ class Search extends Component {
         axios.get(url) 
         .then((response) => {
             // console.log(response.data);
-            const movie=  response.data;
-            let title= movie.Title;
-            let year= movie.Year;
-            let plot= movie.Plot;
+            const movieData=  response.data;
+            let title= movieData.Title;
+            let year= movieData.Year;
+            let plot= movieData.Plot;
+            let movie = {title:title, year:year, isNominee:false, id:title+year};
             if(this.state.title !== title) {
                 this.setState({title: title});
                 this.setState({year: year});
                 this.setState({plot: plot});
-                this.display();
+                this.setState({movie: movie});
             }
         });
     }
 
     display() {
+        let movie= this.state.movie;
         if(this.state.title !== '') {
+            // let movie = {title:this.state.title, year:this.state.year};
+            // let isNominated= true
+            // let movie = {title:title, year:year};
+            // let isMovieNominated = async (isNominated, movie) => { 
+            //     isNominated= true
+            //     movie = {title:title, year:year};
+            //     console.log("movie: ", movie);
+            // };
+            // isMovieNominated(isNominated, movie).then( () => {
+            //     for (let i = 0; i < this.state.nominations.length; i++) {
+            //         // console.log("yo");
+            //         if (this.state.nominations[i] !== movie) {
+            //             isNominated= false
+            //         }      
+            //     }
+            //     return isNominated;
+            // });
+            // if(this.isNominatedCheck(movie) === false){
+            //     // console.log("is nominated");
+            //     // this.setState({ nominations: [this.state.nominations,  movie] });
+            //     // this.setState({ nominations: this.state.nominations.concat([movie])});
+            //     // this.setState({isNominated: true});    
+            // }
+          
             return(
                 // <React.Fragment>
                     <div className="container">
@@ -56,9 +83,9 @@ class Search extends Component {
                     <h5>{this.state.year}</h5>
                     <p>{this.state.plot}</p>
                     {/* conditional rendering for remove nomination button */}
-                    {this.state.isNominated
-                        ? <button onClick={() => {this.removeNomination(this.state.title, this.state.year)}}> Remove Nomination</button> 
-                        : <button onClick={() => {this.nominate(this.state.title, this.state.year )}}>Nominate</button>
+                    {movie['isNominee']
+                        ? <button onClick={() => {this.removeNomination(movie)}}> Remove Nomination</button> 
+                        : <button onClick={() => {this.nominate(movie )}}>Nominate</button> 
                     }
          
                     </div>
@@ -68,72 +95,55 @@ class Search extends Component {
 
     }
 
-    // useEffect() {
-    //     // localStorage.setItem("count", count);
-    //     localStorage.setItem('isNominated', isNominated);
-    //     localStorage.setItem('nominations', nominations);
-    // }
-
-
-    nominate(title, year) {
-        console.log("was nominated");
+    nominate(movie) {
         // object of movie
-        let movie = {title:title, year:year};
+        // let movie = {title:title, year:year, isNominee:false};
         console.log("movie: ", movie);
-      
-        // --------
-        // checks if movie is already a nomination, this process uses async function and 
-        // let isMovieNominated = async () => { 
-        //     if(this.state.isNominated === false){
-        //         // this.setState({ nominations: [this.state.nominations,  movie] });
-        //         this.setState({ nominations: this.state.nominations.concat([movie])});
-        //         console.log("nomination: ", this.state.nominations, "typeOf: ", typeof(this.state.nominations));
-        //     }
-        // };
-        // isMovieNominated().then( () => {
-            
-        //     for (let i = 0; i < this.state.nominations.length; i++) {
-        //         console.log("yo");
-        //         if (this.state.nominations[i] === movie) {
-        //             console.log("yo-yo");
-        //             this.setState({isNominated: true});
-        //         }      
-        //     }
-        // });
-        // ---
-      
-        if(this.state.isNominated === false){
-            // this.setState({ nominations: [this.state.nominations,  movie] });
-            this.setState({ nominations: this.state.nominations.concat([movie])});
-            this.setState({isNominated: true});
-            // console.log("nomination: ", this.state.nominations, "typeOf: ", typeof(this.state.nominations));
-        }
-        // this.nominationList();
+        // this.setState({isNominated: {movie: true}});
+        this.setState({movie: {isNominee: true}});
+        this.setState({ nominations: this.state.nominations.concat([movie])});
+
+        // if(this.isNominatedCheck(movie) === false){
+        //     console.log("is nominated");
+        //     // this.setState({ nominations: [this.state.nominations,  movie] });
+        //     this.setState({ nominations: this.state.nominations.concat([movie])});
+        //     // this.setState({isNominated: true});    
+        // }
     }
+    // isNominatedCheck(movie) {
+    //     let i= 0;
+    //     while(i < (this.state.nominations.length + 1)) {
+    //         // console.log("yo");
+    //         if (this.state.nominations[i] === movie) {
+    //             return true;
+    //         }
+    //         i++;
+    //     }
+    //     return false;
+    // } 
+  
     nominationList() {
         let thisKeyword= this;
         let array= this.state.nominations;
         console.log("isNominated: ", this.state.isNominated);
         console.log("nomination: ", this.state.nominations, "typeOf: ", typeof(this.state.nominations));
         // this.Print(array); 
+        
+        // console.log("nomination value: ", this.state.isNominated['movie'], "typeOf: ", typeof(this.state.isNominated.value));
+        // console.log("nomination object: ", this.state.isNominated, "typeOf: ", typeof(this.state.isNominated));
     }
 
-    // Print(array) {
-    //     return(
-    //         array.map(( nominee) => (
-    //             <tr>{nominee.title} {nominee.year}</tr>
-    //         ))
-    //     );
-    // }
-
-    removeNomination(title, year) {
-        this.nominationList();
+    removeNomination(movie) {
+        // let movie = {title:title, year:year};
+        // this.nominationList();
         // this.setState({ nominations: [this.state.nominations,  movie] }); 
-       let index = this.state.nominations.findIndex(x => x.title === title);
-       console.log("index value: ", this.state.nominations[index])
+        
+        let index = this.state.nominations.findIndex(x => x['id'] === movie['id']);
+        console.log("index value: ", this.state.nominations[index]);
         const newList = this.state.nominations.splice(index, 1);
         this.setState({nominations: newList});
-        this.setState({isNominated: false});
+        this.setState({movie: {isNominee: false}});
+        // this.setState({isNominated: {movie:false}});
         // this.nominationList();
     }
 
@@ -147,6 +157,7 @@ class Search extends Component {
                     </div>
                     <button type="submit" className="btn btn-primary">Submit</button>
             </form>
+
             {this.display()}
 
             <button onClick={()=> {this.nominationList()}} type="button" className="btn btn-primary">View nominations</button>
